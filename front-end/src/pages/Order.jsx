137841176e61615a163bb7e48b83335b5c6eaf9d
Mohammed Hussain <Mohammedhussain7846@gmail.com>
2025-08-12 19:26:40 +0530

    Update Order.jsx

--- front-end/src/pages/Order.jsx
+++ front-end/src/pages/Order.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
-import '../styles/Order.css';
+import '../styles/order.css';
 import SimpleNavbar from '../components/Navbar';
 import { useNavigate } from 'react-router-dom';
 
