2fc57b7804560c1518141d08ddcf1e3aa036c39d
Mohammed Hussain <Mohammedhussain7846@gmail.com>
2025-08-12 19:05:55 +0530

    Update Login.jsx

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
 
   const onSubmit = async (data) => {
     try {
-      const response = await axios.post('http://localhost:4000/login', {
+      const response = await axios.post('https://baghub-by-mohammed.onrender.com/login', {
         email: data.email,
         password: data.password
       });
