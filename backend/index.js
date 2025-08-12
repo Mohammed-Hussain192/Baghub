33e8c99dbe8d2df47fe30535f18998c51206e460
Mohammed Hussain <Mohammedhussain7846@gmail.com>
2025-08-12 20:35:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -26,10 +26,8 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname, "dist")));
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
+app.use(express.static(path.join(__dirname, "public")));
+
 
 
 app.get("/get", async function  (req, res) {
